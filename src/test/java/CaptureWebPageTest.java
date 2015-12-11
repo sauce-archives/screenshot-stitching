4f3c0dfe7ff1d3dec601ae3a815881406e565f89
Malcolm Crum <mcrum@testobject.com>
2015-12-11 13:18:05 +0100

    Chop test up into 10 screenshot chunks

--- src/test/java/CaptureWebPageTest.java
+++ src/test/java/CaptureWebPageTest.java
@@ -3,6 +3,8 @@ import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -10,21 +12,37 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Appium test to screen capture an entire website and save it as a PNG.
  */
+@RunWith(Parameterized.class)
 public class CaptureWebPageTest {
 	private static final String APPIUM_SERVER = getEnvOrDefault("APPIUM_SERVER", "https://app.testobject.com:443/api/appium/wd/hub");
 	private static final String TESTOBJECT_DEVICE = getEnvOrDefault("TESTOBJECT_DEVICE", "iPhone_6S_Plus_16GB_real_2");
-	private static final String TESTOBJECT_APPIUM_VERSION = getEnvOrDefault("TESTOBJECT_APPIUM_VERSION", "1.3.7");
+	private static final String TESTOBJECT_APPIUM_VERSION = getEnvOrDefault("TESTOBJECT_APPIUM_VERSION", "1.4.16");
 	private static String TESTOBJECT_API_KEY = getEnvOrDefault("TESTOBJECT_API_KEY", "");
 	private static String TESTOBJECT_APP_ID = getEnvOrDefault("TESTOBJECT_APP_ID", "6");
 
 	private static TestObjectRemoteWebDriver driver;
+	private final List<String> websites;
 
-	public CaptureWebPageTest() {
+	public CaptureWebPageTest(List<String> websites) {
+		this.websites = websites;
+	}
+
+	@Parameterized.Parameters
+	public static Iterable getWebsiteLists() {
+		List<List<String>> websiteLists = new ArrayList<List<String>>();
+		List<String> allWebsites = Websites.list();
+		int subListSize = 10;
+		for (int i = 0; i < allWebsites.size(); i += subListSize) {
+			int toIndex = i + subListSize < allWebsites.size() ? i + subListSize : allWebsites.size() - 1;
+			websiteLists.add(allWebsites.subList(i, toIndex));
+		}
+		return websiteLists;
 	}
 
 	@Before
@@ -56,7 +74,6 @@ public class CaptureWebPageTest {
 
 	@Test
 	public void openWebPageAndTakeScreenshot() throws Exception {
-		List<String> websites = Websites.list();
 		for (int i = 0; i < websites.size(); ++i){
 			String url = websites.get(i);
 			System.out.println(" Capturing screenshots of " + url + "(" + i + "/" + websites.size() + ")");
